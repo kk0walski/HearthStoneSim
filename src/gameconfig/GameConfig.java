108e891493a15b484cc2a4d9ac53de7bb5e01279
karol <karol.kowalski95@gmail.com>
2019-03-14 23:17:32 +0100

    Add CARDS_IN_PACK final

--- src/gameconfig/GameConfig.java
+++ src/gameconfig/GameConfig.java
@@ -4,6 +4,6 @@ public class GameConfig {
     public static final int CARD_MAX_ATTACK = 12;
     public static final int CARD_MAX_HEALTH = 12;
     public static final int CARD_MAX_MANA = 10;
-
+    public static final int CARDS_IN_PACK = 20;
     public static final int CARDS_IN_DECK = 20;
 }
